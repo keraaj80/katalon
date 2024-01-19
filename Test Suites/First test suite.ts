0cbc09b9e8de0543503b8abbcdc6bc37220ed354
raa10442 <raa10442@rraaj1.esri.com>
2024-01-19 15:04:55 -0800

    Second commit

---
+++ Test Suites/First test suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>First test suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>573eba13-1e73-4e46-927c-e984d6b41956</testSuiteGuid>
+</TestSuiteEntity>
